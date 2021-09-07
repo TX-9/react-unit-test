42637b08373fb0cd7e2ca761a862991fd18d9a60
TX-9 <jacobleecs@gmail.com>
2021-09-07 16:21:16 -0400

    use Wraper class

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
     const [ changedText, setChangedText ] = useState(false);
@@ -9,8 +10,8 @@ const Greeting = () => {
     return (
         <div>
             <h2>Hello World!</h2>
-            { !changedText && <p>Good to see you</p> }
-            { changedText && <p>Changed!</p> }
+            { !changedText && <Output>Good to see you</Output> }
+            { changedText && <Output>Changed!</Output> }
             <button onClick={changeTextHandler}>Click Text</button>
         </div>
     );
